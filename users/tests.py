0fb9fbb58956fe5054aa9eee462efa57e15e027c
DongIkkk <w8h0412@gmail.com>
2022-11-15 09:19:19 +0900

    signup message test code

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class UserSignUpTest(APITestCase):
+    def test_signup(self):
+        url = reverse("user_view")
+        user_data = {
+            "username":"testuser",
+            "fullname":"testuser1",
+            "email":"test@test.com",
+            "password":"password",
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.data["message"], "가입 완료!!")
+
